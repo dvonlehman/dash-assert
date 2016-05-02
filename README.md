363407dc5877546cc78d31610e17dd9d9b8be047
David Von Lehman <david.vonlehman@gmail.com>
2016-05-01 17:48:16 -0700

    Moving to individual function lodash dependencies

--- README.md
+++ README.md
@@ -31,49 +31,49 @@ it('returns an array', function() {
 ## API
 
 ### assert.isNumber(value)
-Assert that that value is a number using [`_.isNumber`](https://lodash.com/docs#isNumber).
+Assert that that value is a number using [`isNumber`](https://lodash.com/docs#isNumber).
 
 ### assert.isNaN(value)
-Assert that value is NaN using [`_.isNaN`](https://lodash.com/docs#isNaN).
+Assert that value is NaN using [`isNaN`](https://lodash.com/docs#isNaN).
 
 ### assert.isObject(value)
-Assert that value is an object using [`_.isObject`](https://lodash.com/docs#isObject).
+Assert that value is an object using [`isObject`](https://lodash.com/docs#isObject).
 
 ### assert.isString(value)
-Assert that value is a astring using [`_.isString`](https://lodash.com/docs#isString).
+Assert that value is a astring using [`isString`](https://lodash.com/docs#isString).
 
 ### assert.isArray(value)
-Assert that value is an array using [`_.isArray`](https://lodash.com/docs#isArray).
+Assert that value is an array using [`isArray`](https://lodash.com/docs#isArray).
 
 ### assert.isEmpty(value)
-Assert that value is empty using [`_.isEmpty`](https://lodash.com/docs#isEmpty).
+Assert that value is empty using [`isEmpty`](https://lodash.com/docs#isEmpty).
 
 ### assert.isNotEmpty(value)
-Assert that the value is not empty using the inverse of [`_.isEmpty`](https://lodash.com/docs#isEmpty).
+Assert that the value is not empty using the inverse of [`isEmpty`](https://lodash.com/docs#isEmpty).
 
 ### assert.isError(value)
-Assert that the specified value in an `Error` type using [`_.isError`](https://lodash.com/docs#isError).
+Assert that the specified value in an `Error` type using [`isError`](https://lodash.com/docs#isError).
 
 ### assert.isNull(value)
-Assert that the value is null using [`_.isNull`](https://lodash.com/docs#isNull).
+Assert that the value is null using [`isNull`](https://lodash.com/docs#isNull).
 
 ### assert.isNotNull(value)
-Assert that the value is null using the inverse of [`_.isNull`](https://lodash.com/docs#isNull).
+Assert that the value is null using the inverse of [`isNull`](https://lodash.com/docs#isNull).
 
 ### assert.isUndefined(value)
-Assert that the specified value is undefined based on [`_.isUndefined`](https://lodash.com/docs#isUndefined).
+Assert that the specified value is undefined based on [`isUndefined`](https://lodash.com/docs#isUndefined).
 
 ### assert.isDefined(value)
-Assert that the specified value is not undefined based on the inverse of [`_.isUndefined`](https://lodash.com/docs#isUndefined).
+Assert that the specified value is not undefined based on the inverse of [`isUndefined`](https://lodash.com/docs#isUndefined).
 
 ### assert.some(collection, predicate)
-Assert that the predicate returns truthy for __any__ element in the collection using [`_.some`](https://lodash.com/docs#some).
+Assert that the predicate returns truthy for __any__ element in the collection using [`some`](https://lodash.com/docs#some).
 
 ### assert.every(collection, predicate)
-Assert that the predicate returns truthy for __all__ elements of the collection using [`_.every`](https://lodash.com/docs#every).
+Assert that the predicate returns truthy for __all__ elements of the collection using [`every`](https://lodash.com/docs#every).
 
 ### assert.isMatch(object, source)
-Assert that all properties of the source are present on the object with equivalent values using [`_.isMatch`](https://lodash.com/docs#isMatch).
+Assert that all properties of the source are present on the object with equivalent values using [`isMatch`](https://lodash.com/docs#isMatch).
 
 ### assert.isTrue(value)
 Shortcut for `assert.strictEqual(value, true)`.
@@ -82,13 +82,13 @@ Shortcut for `assert.strictEqual(value, true)`.
 Shortcut for `assert.strictEqual(value, false)`.
 
 ### assert.hasIntersect(array, otherArray)
-Assert that there is at least one value in common between the two arrays based on [`_.intersection`](https://lodash.com/docs#intersection) having a length greater than 0.
+Assert that there is at least one value in common between the two arrays based on [`intersection`](https://lodash.com/docs#intersection) having a length greater than 0.
 
 ### assert.noIntersect(array, otherArray)
-Assert that the two arrays do not intersect based on [`_.intersection`](https://lodash.com/docs#intersection) having a length equal to 0.
+Assert that the two arrays do not intersect based on [`intersection`](https://lodash.com/docs#intersection) having a length equal to 0.
 
 ### assert.noDifferences(array, otherArray)
-Assert that there are no differences between the two arrays based on [`_.difference`](https://lodash.com/docs#difference) having a length of 0.
+Assert that there are no differences between the two arrays based on [`difference`](https://lodash.com/docs#difference) having a length of 0.
 
 ### assert.matchesPattern(value, regex)
 Assert that the value passes a RegExp test.
--- index.js
+++ index.js
@@ -1,123 +1,137 @@
-var assert = require("assert"),
-  _ = require('lodash');
+var assert = require("assert");
+var isNumber = require('lodash.isnumber');
+var isString = require('lodash.isstring');
+var isEmpty = require('lodash.isempty');
+var isArray = require('lodash.isarray');
+var isNan = require('lodash.isnan');
+var isNull = require('lodash.isnull');
+var isUndefined = require('lodash.isundefined');
+var isObject = require('lodash.isobject');
+var isMatch = require('lodash.ismatch');
+var isError = require('lodash.iserror');
+var every = require('lodash.every');
+var some = require('lodash.some');
+var isRegExp = require('lodash.isregexp');
+var intersection = require('lodash.intersection');
+var difference = require('lodash.difference');
 
 assert.isNumber = function(value) {
-  if (_.isNumber(value) === false)
+  if (isNumber(value) === false)
     throw new assert.AssertionError({message: "value is not a number"});
 };
 
 assert.isString = function(value) {
-  if (_.isString(value) === false)
+  if (isString(value) === false)
     throw new assert.AssertionError({message: "value is not a string"});
 };
 
 // Assert that the value is NaN. See https://lodash.com/docs#isNaN
 assert.isNaN = function(value) {
-  if (_.isNaN(value) === false)
+  if (isNaN(value) === false)
     throw new assert.AssertionError({message: "value is not NaN"});
 }
 
 // Assert the value is an object.
 // See https://lodash.com/docs#isObject
 assert.isObject = function(val) {
-  if (_.isObject(val) === false)
+  if (isObject(val) === false)
     throw new assert.AssertionError({message: "value is not an object"});
 };
 
 // Assert that all properties of the source are present on the object with equivalent values.
 // See https://lodash.com/docs#isMatch
 assert.isMatch = function(object, source) {
-  if (_.isMatch(object, source) === false)
+  if (isMatch(object, source) === false)
     throw new assert.AssertionError({message: "Source properties are not a match"});
 };
 
 // Assert that the specified value is an Error type.
 // See https://lodash.com/docs#isError
 assert.isError = function(value) {
-  if (_.isError(value) === false)
+  if (isError(value) === false)
     throw new assert.AssertionError({message: "Value is not an Error"});
 };
 
 // Assert that both arrays are the same length and contain the same values,
 // though not necessarily in the same order. See https://lodash.com/docs#difference
 assert.noDifferences = function(array, otherArray) {
-  if (!(_.isArray(array) && _.isArray(otherArray)))
+  if (!(isArray(array) && isArray(otherArray)))
     throw new Error("Both arguments must be arrays");
 
   if (array.length !== otherArray.length)
     throw new assert.AssertionError({message: "Arrays are not the same length"});
 
-  if (_.difference(array, otherArray).length > 0)
+  if (difference(array, otherArray).length > 0)
     throw new assert.AssertionError({message: "Arrays have differences"});
 };
 
 // Assert that the two arrays intersect.
 assert.hasIntersect = function(array, otherArray) {
-  if (!(_.isArray(array) && _.isArray(otherArray)))
+  if (!(isArray(array) && isArray(otherArray)))
     throw new Error("Both arguments must be arrays");
 
-  if (_.intersection(array, otherArray).length === 0)
+  if (intersection(array, otherArray).length === 0)
     throw new assert.AssertionError({message: "Arrays do not intersect"});
 };
 
 // Assert that the two arrays have no intersection.
 assert.noIntersect = function(array, otherArray) {
-  if (!(_.isArray(array) && _.isArray(otherArray)))
+  if (!(isArray(array) && isArray(otherArray)))
     throw new Error("Both arguments must be arrays");
 
-  if (_.intersection(array, otherArray).length > 0)
+  if (intersection(array, otherArray).length > 0)
     throw new assert.AssertionError({message: "Arrays intersect"});
 }
 
 // Assert that the value is an array. See https://lodash.com/docs#isArray
 assert.isArray = function(val) {
-  if (_.isArray(val) === false)
+  if (isArray(val) === false)
     throw new assert.AssertionError({message: "Value is not an array"});
 };
 
 assert.isEmpty = function(value) {
-  if (_.isEmpty(value) === false)
+  if (isEmpty(value) === false)
     throw new assert.AssertionError({message: "value is not empty"});
 };
 
 assert.isNotEmpty = function(value) {
-  if (_.isEmpty(value) === true)
+  if (isEmpty(value) === true)
     throw new assert.AssertionError({message: "value is empty"});
 };
 
 assert.isDefined = function(val) {
-  if (_.isUndefined(val) === true)
+  if (isUndefined(val) === true)
     throw new assert.AssertionError({message: "Expected value to be defined"});
 };
 
 assert.isUndefined = function(val) {
-  if (_.isUndefined(val) !== true)
+  if (isUndefined(val) !== true)
     throw new assert.AssertionError({message: "Expected value to be undefined"});
 };
 
 // Assert that the value is not null. See https://lodash.com/docs#isNull
 assert.isNotNull = function(value) {
-  if (_.isNull(value) === true)
+  if (isNull(value) === true)
     throw new assert.AssertionError({message: "value is null"});
 };
 
 // Assert that the value is null. See https://lodash.com/docs#isNull
 assert.isNull = function(value) {
-  if (_.isNull(value) === false)
+  if (isNull(value) === false)
     throw new assert.AssertionError({message: "value is not null"});
 };
 
 // Assert that predicate returns truthy for any element of collection.
 // See https://lodash.com/docs#some
 assert.some = function(collection, predicate) {
-  if (_.some(collection, predicate) !== true)
+  if (some(collection, predicate) !== true)
     throw new assert.AssertionError({message: "No matching value in the collection"});
 };
 
 // Assert that predicate returns truthy for every element of collection.
 // See https://lodash.com/docs#every
 assert.every = function(collection, predicate) {
-  if (_.every(collection, predicate) !== true)
+  if (every(collection, predicate) !== true)
     throw new assert.AssertionError({message: "Not all the values in the collection pass the test"});
 };
 
@@ -135,10 +149,10 @@ assert.isFalse = function(value) {
 
 // Assert that the value passes the regex test.
 assert.matchesPattern = function(value, regex) {
-  if (_.isString(value) === false)
+  if (isString(value) === false)
     throw new Error("value is not a string");
 
-  if (_.isRegExp(regex) === false)
+  if (isRegExp(regex) === false)
     throw new Error("regex is not a RegExp");
 
   if (regex.test(value) !== true)
@@ -148,7 +162,7 @@ assert.matchesPattern = function(value, regex) {
 // Assert that the value is a valid JSON string.
 assert.isJSON = function(val) {
   var valid = false;
-  if (_.isString(val)) {
+  if (isString(val)) {
     try {
       JSON.parse(val);
       valid = true;
